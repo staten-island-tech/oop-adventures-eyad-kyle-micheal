c64aed01acfd39e0bc8b7b158f696c2c26c6111b
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-11-16 14:00:17 -0500

    Update weapon.py

---
+++ weapon.py
@@ -0,0 +1,30 @@
+class weapon():
+    def __init__(self, name, damage, durability):
+        self.damage = damage
+        self.durability = durability
+
+flimsy_wooden_sword = ("Flimsy Wooden Sword", 30, 2)
+
+flimsy_steel_sword = ("Flimsy Steel Sword", 50, 2)
+
+sturdy_wooden_sword = ("Sturdy Wooden Sword", 35, 3)
+
+sturdy_steel_sword = ("Flimsy Steel Sword", 60, 5)
+
+flimsy_wooden_staff = ("Flimsy Wooden Staff", 30 , 2)
+
+flimsy_steel_staff = ("Flimsy Wooden Staff", 50 , 2)
+
+sturdy_wooden_staff = ("Sturdy Wooden Staff", 35 , 3)
+
+sturdy_steel_staff = ("Sturdy Steel Staff", 60 , 5)
+
+flimsy_wooden_bow = ("Flimsy Wooden Bow", 30, 2)
+
+flimsy_steel_bow = ("Flimsy Steel Bow", 50, 2)
+
+sturdy_wooden_bow = ("Sturdy Wooden Bow", 35, 3)
+
+sturdy_steel_bow = ("Sturdy Steel Bow", 60, 5)
+
+print(sturdy_steel_bow)
