48dabd07c902460f5938be44b154c74f67938ce7
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-11-16 14:00:15 -0500

    Update armor.py

---
+++ armor.py
@@ -0,0 +1,13 @@
+class armor():
+    def __init__(self, name, defense, durability):
+        self.name = name
+        self.defense = defense
+        self.durability = durability
+
+flimsy_leather_armor = ("Flimsy Leather Armor", 30, 3)
+
+flimsy_steel_armor = ("Flimsy Steel Armor", 50, 4)
+
+print(flimsy_steel_armor)
+
+
