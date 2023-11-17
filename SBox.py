4751df71d1997cac555407061b7249c2919c1844
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2023-11-17 13:38:03 -0500

    Update SBox.py

--- SBox.py
+++ SBox.py
@@ -1,6 +1,6 @@
 
 
-class SkillCommon():
+class PasCommon():
     def c(self, srandint, skill_health, entity_health):
         import random
         srandint = random.randint(1, 50)
@@ -8,7 +8,7 @@ class SkillCommon():
         self.skill_health = skill_health
         self.entity_health = entity_health
 
-class Pas(SkillCommon):
+class Pas(PasCommon):
     def Regen(self, srandint, skill_health, entity_health, regained):
         super().c(self, srandint, skill_health, entity_health)
         self.regained = regained
@@ -25,6 +25,33 @@ class Pas(SkillCommon):
 
 
 
+
+# Do Later 👇
+#class ActCommon():
+#    def k(self, srandint, skill_health, entity_health):
+#        import random
+#        srandint = random.randint(1, 5)
+#        self.srandint = srandint
+#        self.skill_health = skill_health
+#        self.entity_health = entity_health
+
+#class Act(ActCommon):
+#    def ss(self, srandint, skill_health, entity_health):
+#        super().k
+#
+#    def bb(self, srandint, skill_health, entity_health):
+#
+#
+#    def usa(self, srandint, skill_health, entity_health):
+
+
+
+
+
+
+
+# Testing below here 😭 👇 |
+
 class Dummy():
     def __init__(self, health):
         self.health = health
@@ -34,5 +61,7 @@ print(TestDummy.__dict__)
 
 bleed = Pas.Bleed
 print(bleed.__dict__)
+
+
 # Note for future self because I WILL forget:
 # I need to find a way to use get a passive move like bleed or regen to affect an entity
