7ed1ff09a6d20d8d8a6bd1b803172bcfe888b9c4
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2023-11-20 13:55:49 -0500

    fyuknjryk

--- testing.py
+++ testing.py
@@ -6,14 +6,16 @@ class TestAttack():
         self.name = name
         self.base_damage = base_damage
 quick_attack = TestAttack("quick_attack",120)
+coins = 7
 def attack():
-    while goblin.health != 0:
+    while goblin.health > 0:
         x=input("what attack:")
         if x == ("quick attack"):
-            goblin.health = goblin.health -(quick_attack.base_damage-(quick_attack.base_damage*goblin.defense))
+            goblin.health = max(0,goblin.health -(quick_attack.base_damage-(quick_attack.base_damage*goblin.defense)))
             print(goblin.__dict__)
         else:
             print(input("sorry thats not an attack try again"))
     if goblin.health ==0 or goblin.health <0:
         print("youve defeated the enemy,move on")
+        print(f"you have gained {coins} coins")
 attack()
