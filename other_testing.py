6b08a24010ced8797afde1b69413d75f116da632
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-11-21 14:00:47 -0500

    Create other_testing.py

---
+++ other_testing.py
@@ -0,0 +1,28 @@
+from enemies import Enemies
+from player import Player
+from weapon import Weapons
+goblin = Enemies("Goblin",100,0.4,0.8,100)    
+archer = ("Archer",90,  .35,  1.3,  0.8,  95,  1.1,  15)
+
+class TestAttack():
+    def __init__(self,name,base_damage):
+        self.name = name
+        self.base_damage = base_damage
+    
+
+quick_attack = TestAttack("quick_attack",120)
+coins = 7
+
+def attack(enemy,player):
+    while enemy.health > 0:
+        x=input("what attack:")
+        if x == ("quick attack"):
+            enemy.health = max(0,enemy.health -(quick_attack.base_damage-(quick_attack.base_damage*enemy.defense)))
+            print(enemy.__dict__)
+        else:
+            print(input("sorry thats not an attack try again"))
+    if enemy.health ==0 or enemy.health <0:
+        print("youve defeated the enemy,move on")
+        print(f"you have gained {coins} coins")
+
+attack(goblin,archer)
