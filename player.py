84fe662f2d63d8f30b63a7ecdf4f1d6e2f199bca
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2023-11-27 13:58:04 -0500

    ftyuuyd65sdr

--- player.py
+++ player.py
@@ -10,12 +10,11 @@ class Player():
         self.magic_talent = magic_talent
         self.money = money
 class Archer(Player):
-    def __init__(self):
-        super().__init__("Archer",90,  .35,  1.3,  0.8,  95,  1.1,  15) 
-class Assasin(Player):
-    def __init__(self,stealth ):
-        super().__init__("assasin", 100, 90, 90, 150, 100, 25, 0)
-        self.stealth = stealth
-assasin = Assasin
-print(assasin.__dict__)
+    def __init__(self,name,health,melee_attack,ranged_attack,speed,intelligence,magic_talent,money):
+        super().__init__(name,health,melee_attack,ranged_attack,speed,intelligence,magic_talent,money) 
 
+class Assasin(Player):
+    def __init__(self,name,health,melee_attack,ranged_attack,speed,intelligence,magic_talent,money,stealth ):
+        super().__init__(name,health,melee_attack,ranged_attack,speed,intelligence,magic_talent,money)
+        self.stealth = stealth
+assasin=Assasin("assasin", 100, 90, 90, 150, 100, 25, 0,1000)
--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 from enemies import Enemies
-from player import Player
+from player import Player,Archer
 goblin = Enemies("Goblin",100,0.4,0.8,100)    
 archer = ("Archer",90,  .35,  1.3,  0.8,  95,  1.1,  15) 
 
@@ -11,14 +11,13 @@ class TestAttack():
         return self.base_damage-(self.base_damage*enemy.defense)
 quick_attack = TestAttack("quick_attack",120)
 coins = 7
+archer = Archer("Archer",90,  .35,  1.3,  0.8,  95,  1.1,  15)
+def choose_your_class():
+    choose = input("what:")
+    if choose == ("archer"):
+        print(f"youre chosen class is {choose}")
 
-
-        
-
-
-print(archer)
-
-
+choose_your_class()
 def attack(enemy):
     while enemy.health > 0:
         x=input("what attack:")
@@ -27,7 +26,7 @@ def attack(enemy):
             enemy.health = max(0,enemy.health - damage)
             print(enemy.__dict__)
         else:
-            print(input(f"sorry thats not an attack try again {x}"))
+            print(input("sorry thats not an attack try again "))
     if enemy.health <=0:
         print("youve defeated the enemy,move on")
         print(f"you have gained {coins} coins")
