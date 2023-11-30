78c49f9a0bde6792011a54e4a5c641f9a0b60561
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-11-30 14:00:21 -0500

    Update merchant_vendor.py

--- merchant_vendor.py
+++ merchant_vendor.py
@@ -1,9 +1,36 @@
+coins = 100
+
+shop_inventory = ["Health Potion", "Flimsy Wooden Sword"]
+inventory = []
 class Merchant:
-    def __init__(self, name, product):
+    def __init__(self, name, cost):
         self.name = name
-        self.product = product
-    def sell(self, item):
-        self.product.remove(item)
-        print(f"you have purchased" (item))
-        print(self.products)
+        self.cost = cost
+
+stage_1 = 1
+stage_2 = 2
+def sell_2():
+    while stage_2 == 2:
+        redo = input("Do you want to buy anything else? (Yes or No): ")
+    if redo == "Yes":
+        sell_1()
+
+def sell_1():
+    print(shop_inventory)
+    while stage_1 == 1:
+        user_purchase = input("What would you like to buy?: ")
+        if user_purchase == "Health Potion":
+            print("You have purchased a Heath Potion")
+            shop_inventory.remove("Health Potion")
+            inventory.append("Health Potion")
+            print(inventory)
+            sell_2()
+        else:
+            print("Incorrect Input, Please try again")
+sell_1()
+
+
+
+
+health_potion = Merchant("Health Potion", 10)
 
