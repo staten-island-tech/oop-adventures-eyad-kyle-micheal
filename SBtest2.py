1988d8ce9b042a9da761a76be766015ac26770e6
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2023-11-30 13:59:15 -0500

    aaa

--- SBtest2.py
+++ SBtest2.py
@@ -9,7 +9,8 @@ Regeneration = PasCommon.c('Regen', 0.2)
 Regained = (Regeneration * passiverandom)
 Bleeding = PasCommon.c('Bleed', 0.3)
 Bled = (Bleeding * passiverandom)
-
+# Bled is Bleed damage, subtract it from health
+# Regained is health healed, add it to health
 
 class Dummy():
     def __init__(self, health):
@@ -21,23 +22,57 @@ print(TestDummy.__dict__)
 
 
 
-def SB():
+def SkillBleed():
     import random
     Chance = random.randint(1, 10)
-    print(Chance)
     if Chance < 8 or Chance == 8:
         TestDummy.health = TestDummy.health - Bled
     elif Chance > 8:
         print('Failed')
     else:
         print('Broke lol | Its the code,not you 🤣')
-SB()
 
-while TestDummy.health != 0:
+
+
+
+def SkillHeal():
+    import random
+    Chance = random.randint(1, 10)
+    if Chance < 8 or Chance == 8:
+        TestDummy.health = TestDummy.health + Regained
+    elif Chance > 8:
+        print('Failed')
+    else:
+        print('Broke lol | Its the code,not you 🤣')
+
+def SkillPierce():
+    import random
+    Chance = random.randint(1, 10)
+    if Chance < 8 or Chance == 8:
+        TestDummy.health = TestDummy.health - round(Piercing_Slash)
+    elif Chance > 8:
+        print('Failed')
+    else:
+        print('Broke lol | Its the code,not you 🤣')
+
+
+while TestDummy.health > 0:
     ui = input('Test skill? Y/N ')
     if ui.lower() == 'y':
-        SB()
-        print(TestDummy.__dict__)
+        print('Piercing...')
+        SkillPierce()
+        round(TestDummy.health)
+        print(TestDummy.health)
+
+        print('Bleeding...')
+        SkillBleed()
+        round(TestDummy.health)
+        print(TestDummy.health)
+
+        print('Healing...')
+        SkillHeal()
+        round(TestDummy.health)
+        print(TestDummy.health)
     elif ui.lower() == 'n':
         break
     else:
