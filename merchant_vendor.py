4c52c71cfe632660dac684a307758a0529451339
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-11-30 13:29:46 -0500

    Update merchant_vendor.py

---
+++ merchant_vendor.py
@@ -0,0 +1,9 @@
+class Merchant:
+    def __init__(self, name, product):
+        self.name = name
+        self.product = product
+    def sell(self, item):
+        self.product.remove(item)
+        print(f"you have purchased" (item))
+        print(self.products)
+
