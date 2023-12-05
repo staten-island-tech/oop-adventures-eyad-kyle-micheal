1b610623573ae7b4a5298e6b99112848697ed633
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-12-05 13:59:19 -0500

    Update merchant_vendor.py

--- merchant_vendor.py
+++ merchant_vendor.py
@@ -1,9 +1,13 @@
-amount = 100
-coins = 100
+cost_1 = 10
+cost_2 = 30
+amount = 10
 
-if amount == 0:
-    print("no money")
-    exit()
+def user_warning(amount):
+    if amount < 0:
+        print("You do not have enough money to afford this")
+        amount+=cost_2
+        sell_1(amount)
+
 def leave_shop():
     exit()
 
@@ -12,9 +16,6 @@ inventory = []
 class mooney():
     def __init__(self, amount):
         self.amount = amount
-    #Merchant:
-    #def __init__(self, name, cost):
-        ##self.cost = cost
 
 stage_1 = 1
 stage_2 = 2
@@ -38,22 +39,29 @@ def sell_1(amount):
             print("Incorrect Input, Item is not in shop")
             sell_1(amount)
         if user_purchase == "Health Potion":
+            amount-=cost_1
+            user_warning(amount)
             print("You have purchased a Heath Potion")
             shop_inventory.remove("Health Potion")
             inventory.append("Health Potion")
-            amount-=100
-            print(amount)
             print(inventory)
-
+            print(amount)
+            if amount == 0:
+                print("You ran out of money!")
+                exit()
             sell_2(amount)
         if user_purchase == "Flimsy Wooden Sword":
-            print("You have purchased a Flimsy Wooden Sword")
-            shop_inventory.remove("Flimsy Wooden Sword")
-            inventory.append("Flimsy Wooden Sword")
-            amount-=10
-            print(amount)
-            print(inventory)
-            sell_2(amount)
+            amount-=cost_2
+            user_warning(amount)
+        print("You have purchased a Flimsy Wooden Sword")
+        print(inventory)
+        print(amount)
+        shop_inventory.remove("Flimsy Wooden Sword")
+        inventory.append("Flimsy Wooden Sword")
+        if amount == 0:
+            print("You ran out of money")
+            exit()
+    sell_2(amount)
 sell_1(amount)
 
 
