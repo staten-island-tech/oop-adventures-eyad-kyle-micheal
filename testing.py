362945460c08163c6128703222f5b0a7bfafacc1
mellowed24 <eyadabdelfatah6@gmail.com>
2023-12-07 17:50:44 -0500

    lknlnljb

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
 from enemies import Enemies,Whalen
 from player import Player,Archer,Assasin,Warrior,Berserker,Fighter,Wizard
 import random
+chosen = Player.better_choose_class
+chosen_use=chosen()
 #all enemies
 goblin = Enemies("Goblin","a little green thing;embarrising if you die to it",100,0.4,0.8,100)   
 troll = Enemies("Troll","a slightly bigger thing;would be less embarrisiing", 200, 2, 1, 0.05)
@@ -29,14 +31,7 @@ class TestAttack():
     def __str__(self):
         return (f'name:{self.name};damage:{self.base_damage}')
 
-classes_choice=[
-        Archer("Archer",90,  .35,  1.3,  0.8,  95,  1.1,  15,"infinite"),
-        Assasin("Assasin", 100, 90, 90, 150, 100, 25, 0,1000),
-        Warrior("Warrior",150,125,75,100,100,75,0,1000),
-        Berserker("Berserker",100,150,30,75,100,15,0,1000000),
-        Fighter("Fighter",150,100,1,175,100,10,0,100000),
-        Wizard("Wizard",150,10,100,100,150,300,0,10000000),
-]
+
 attacks=[
     TestAttack("fireball",10),
     TestAttack("quick attack",20),
@@ -48,39 +43,8 @@ quick_attack=TestAttack("quick attack",20)
 heavy_attack = TestAttack("heavy attack",30)
 
 
-def print_classes():
-    for i, classes in enumerate (classes_choice):
-        print(f'{i+1}. {classes}  ')
-        print()
-    return i
-number_of_classes = print_classes
-def better_choose_class():
-    while True:
-        try:
-            choose = int(input("what class are you interested in(put the corresponding number):"))
-            number_of_classes=6
-            if number_of_classes>=choose>=1:
-                if choose == 1:
-                    return archer
-                elif choose == 2:
-                    return assasin
-                elif choose==3:
-                    return warrior
-                elif choose == 4:
-                    return Berserker
-                elif choose== 5:
-                    return fighter
-                elif choose == 6:
-                    return wizard
-                else:
-                    print("no1")
-            else:
-                print("It seems this wasn't one of the choices.Please enter a valid number.")
-        except ValueError:
-            print("Please enter an integer")
-            
-            
-chosen = better_choose_class
+
+
 def print_skills():
     for i,skillss in enumerate(attacks):
         print(f'{i+1}:{skillss.__dict__}')
@@ -104,6 +68,7 @@ def choose_skill():
                 print("It seems this wasn't one of the choices.Please enter a valid number.")
         except ValueError:
             print("Please enter an integer")
+
 
 
 
