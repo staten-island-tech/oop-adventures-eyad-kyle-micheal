341be546ddc7234976a51a21551281dfbe9628b2
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-12-13 13:59:53 -0500

    Update merchant_vendor.py

--- merchant_vendor.py
+++ merchant_vendor.py
@@ -1,121 +1,21 @@
-#cost_1 = 10
-#cost_2 = 10
-#cost_3 = 10
-#amount = 20
-
-#def user_warning_1(amount):
-    #if amount < 0:
-        #print("You do not have enough money to afford this")
-        #amount+=cost_1
-        #sell_1(amount)
-
-#def user_warning_2(amount):
-    #if amount < 0:
-        #print("You do not have enough money to afford this")
-        #amount+=cost_2
-        #sell_1(amount)
-
-#def user_warning_3(amount):
-    #if amount < 0:
-        #print("You do not have enough money to afford this")
-        #amount+=cost_3
-        #sell_1(amount)
-
-#shop_inventory = ["Health Potion", "Flimsy Wooden Sword", "Flimsy Steel Sword"]
-#inventory = []
-#class mooney():
-    #def __init__(self, amount):
-        #self.amount = amount
-
-#stage_1 = 1
-#stage_2 = 2
-#def sell_2(amount):
-    #if not shop_inventory:
-      #  print("There is nothing to buy")
-     #   exit()
-    #while stage_2 == 2:
-      #  print(shop_inventory)
-     #   redo = input("Do you want to buy anything else? (Yes or No): ")
-    #    if redo == "Yes":
-   #         sell_1(amount)
-  #      if redo == "No":
- #           exit()
-#
-#def sell_1(amount):
-  #  print(shop_inventory)
- #   while stage_1 == 1:
-#        user_purchase = input("What would you like to buy?(Type exit if you want to leave.): ")
-       # if not shop_inventory:
-      #      print("There is nothing to buy")
-     #       exit()
-    #    if user_purchase == "Exit":
-   #         print("You left the shop...")
-  #          exit()
- #       if user_purchase == "Health Potion":
-#            if amount == 0:
-      #          print("You ran out of money")
-     #           print(inventory)
-    #            exit()
-   #         amount-=cost_1
-  #          user_warning_1(amount)
- #           print("You have purchased a Heath Potion")
-#            shop_inventory.remove("Health Potion")
-      #      inventory.append("Health Potion")
-     #       print(inventory)
-    #        print(amount)
-   #         sell_2(amount)
-  #      if user_purchase == "Flimsy Wooden Sword":
- #           if amount == 0:
-#                print("You ran out of money")
-     #           exit()
-    #        amount-=cost_2
-   #         user_warning_2(amount)
-  #          print("You have purchased a Flimsy Wooden Sword")
- #           shop_inventory.remove("Flimsy Wooden Sword")
-#            inventory.append("Flimsy Wooden Sword")
-      #      print(inventory)
-     #       print(amount)
-    #        sell_2(amount)
-   #     if user_purchase == "Flimsy Steel Sword":
-  #          if amount == 0:
- #               print("You ran out of money")
-#                exit()
-     #       amount-=cost_3
-    #        user_warning_3(amount)
-   #         print("You have purhcased a Flimsy Steel Sword")
-  #          shop_inventory.remove("Flimsy Steel Sword")
- #           inventory.append("Flimsy Steel Sword")
-#            print(inventory)
-    #        print(amount)
-   #         sell_2(amount)
-  #      else:
- #           print("Incorrect Input, Item is not in shop")
-#           sell_1(amount)
-#sell_1(amount)
-
-
-cost_1 = 10
-cost_2 = 40
-cost_3 = 60
-amount = 100
-
-def user_warning_1(amount):
-    if amount < 0:
+coins = 100
+def user_warning_1(coins):
+    if coins < 0:
         print("You do not have enough money to afford this")
-        amount+=cost_1
-        sell_1(amount)
+        coins+=Health_Potion.amount
+        sell_1(coins)
 
-def user_warning_2(amount):
-    if amount < 0:
+def user_warning_2(coins):
+    if coins < 0:
         print("You do not have enough money to afford this")
-        amount+=cost_2
-        sell_1(amount)
+        coins+=Flimsy_Wooden_Sword.amount
+        sell_1(coins)
 
-def user_warning_3(amount):
-    if amount < 0:
+def user_warning_3(coins):
+    if coins < 0:
         print("You do not have enough money to afford this")
-        amount+=cost_3
-        sell_1(amount)
+        coins+=Flimsy_Steel_Sword.amount
+        sell_1(coins)
 
 shop_inventory = ["Health Potion", "Flimsy Wooden Sword", "Flimsy Steel Sword"]
 inventory = []
@@ -123,22 +23,26 @@ class mooney():
     def __init__(self, name, amount):
         self.amount = amount
         self.name = name
-    Health_Potion = ("Health Potion", 10)
-    Flimsy_Wooden_Sword = ("Flimsy Wooden Sword", 40)
-    Flimsy_Steel_Sword = ("Flimsy Steel Sword", 60)
-
+Health_Potion = mooney("Health Potion", 10)
+Flimsy_Wooden_Sword = mooney("Flimsy Wooden Sword", 40)
+Flimsy_Steel_Sword = mooney("Flimsy Steel Sword", 60)
+i=str(Health_Potion.name+Health_Potion.amount)
 stage_1 = 1
-def sell_1(amount):
-    print("This is the amount of money you have:")
-    print(amount)
+def sell_1(coins):
     print("Items for sale:")
     if "Health Potion" in shop_inventory:
-        print("Health Potion Cost: 10")
+        print(Health_Potion.name)
+        print(Health_Potion.amount)
+        print(i)
     if "Flimsy Wooden Sword" in shop_inventory:
-        print("Flimsy Wooden Sword Cost: 40")
+        print(Flimsy_Wooden_Sword.name)
+        print(Flimsy_Wooden_Sword.amount)
     if "Flimsy Steel Sword" in shop_inventory:
-        print("Flimsy Steel Sword Cost: 60")
+        print(Flimsy_Steel_Sword.name)
+        print(Flimsy_Steel_Sword.amount)
     while stage_1 == 1:
+        print("This is the amount of money you have:")
+        print(coins)
         user_purchase = input("What would you like to buy?(Type Exit if you want to leave.): ")
         if not shop_inventory:
             print("There is nothing to buy")
@@ -147,46 +51,46 @@ def sell_1(amount):
             print("You left the shop...")
             exit()
         if user_purchase == "Health Potion":
-            amount-=cost_1
-            user_warning_1(amount)
+            coins-=Health_Potion.amount
+            user_warning_1(coins)
             print("You have purchased a Heath Potion")
             shop_inventory.remove("Health Potion")
             inventory.append("Health Potion")
             print("This is your inventory:")
             print(inventory)
-            if amount == 0:
+            if coins == 0:
                 print("You ran out of money")
                 print("You left the shop...")
                 exit()
-            sell_1(amount)
+            sell_1(coins)
         if user_purchase == "Flimsy Wooden Sword":
-            amount-=cost_2
-            user_warning_2(amount)
+            coins-=Flimsy_Wooden_Sword.amount
+            user_warning_2(coins)
             print("You have purchased a Flimsy Wooden Sword")
             shop_inventory.remove("Flimsy Wooden Sword")
             inventory.append("Flimsy Wooden Sword")
             print("This is your inventory:")
             print(inventory)
-            if amount == 0:
+            if coins == 0:
                 print("You ran out of money")
                 print("You left the shop...")
                 exit()
-            sell_1(amount)
+            sell_1(coins)
         if user_purchase == "Flimsy Steel Sword":
-            amount-=cost_3
-            user_warning_3(amount)
+            coins-=Flimsy_Steel_Sword.amount
+            user_warning_3(coins)
             print("You have purhcased a Flimsy Steel Sword")
             shop_inventory.remove("Flimsy Steel Sword")
             inventory.append("Flimsy Steel Sword")
             print("This is your inventory:")
             print(inventory)
-            if amount == 0:
+            if coins == 0:
                 print("You ran out of money")
                 print("You left the shop...")
                 exit()
-            sell_1(amount)
+            sell_1(coins)
         else:
             print("Incorrect Input, Item is not in shop")
-            sell_1(amount)
-sell_1(amount)
+            sell_1(coins)
+sell_1(coins)
 
