4c16fe1a0baf29e77dc8ab05f2dea1e9e2fed5c0
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2024-01-05 14:00:14 -0500

    rjyw5

--- main_project.py
+++ main_project.py
@@ -2,18 +2,12 @@ from working import Player,Archer,Assasin,Warrior,Berserker,Wizard,Fighter,Enemi
 from SBox import Active,PasCommon,archer_skill,warrior_skill,b_skill,assasin_skill,fighter_skill
 from merchant_vendor import mooney
 
-def tutorial():
-    print("Helo adventurer! You have made it to the tower of chellenges")
-shop = ["Helaht","Damage"]
-inventory=[]
-def printint():
-    for i, it in enumerate(shop):
-        print(f'{i+1},{it}')
+def tutorial_tiny():
+    print("Helo adventurer! You have made it to the tower of chellenges Before doing anything else, you will have to learn the basics. First, you'll have to choose your class")
+Player.print_classes()
+chosen = Player.better_choose_class()
+def actual():
+    print(f"""Great! You've sucessfullay chosen a class ! Here aare your stats 
+          {chosen.__dict__} """)
 
-def adding():
-    printint()
-    y = int(input(""))
-    if 2>=y<=1:
-        inventory.append(shop[y-1])
-adding()
-print(inventory[0])
+actual()
