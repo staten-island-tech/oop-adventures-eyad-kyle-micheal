c5f091897ecd420190c23cc002c8e7b5f76b7161
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2024-01-10 13:59:36 -0500

    asd

--- a.py
+++ a.py
@@ -1,38 +1,28 @@
-<<<<<<< HEAD
-=======
-coins = 100
->>>>>>> 29ab7de321646d27886feaf3186dd8406296a66d
 class mooney:
     def __init__(self, name, amount):
         self.amount = amount
         self.name = name
-<<<<<<< HEAD
-    def buy(x,item):
-     if begin in a:
-            a.remove(str(begin))
+
+    def buy(x,item,item2,item3):
+        if begin in shop_invo:
+            shop_invo.remove(str(begin))
             player.append(str(begin))
             coins = x -item.amount
             print(coins)
-            print(a)
+            print(shop_invo)
             print(player)
+        else:
+            print("no")
+    
 Health_Potion = mooney("Health Potion", 10)
 Flimsy_Wooden_Sword = mooney("Flimsy Wooden Sword", 40)
 Flimsy_Steel_Sword = mooney("Flimsy Steel Sword", 60)
 
-a = [Health_Potion.name, Flimsy_Wooden_Sword.name]
+shop_invo = [Health_Potion.name, Flimsy_Wooden_Sword.name, Flimsy_Steel_Sword.name]
+def silly():
+    if not shop_invo:
+        print("you left")
+        exit()
 player = []
-begin = input("buy item")
-mooney.buy(100,Health_Potion)
-=======
-Health_Potion = mooney("Health Potion", 10)
-Flimsy_Wooden_Sword = mooney("Flimsy Wooden Sword", 40)
-Flimsy_Steel_Sword = mooney("Flimsy Steel Sword", 60)
-shop = [Health_Potion.name, Flimsy_Wooden_Sword.name, Flimsy_Steel_Sword.name]
-def buy():
-    thing=input("buy")
-    if thing in shop:
-        shop.remove(str(thing))
-        print(str(thing.amount))
-        print(shop)
-buy()
->>>>>>> 29ab7de321646d27886feaf3186dd8406296a66d
+begin = input("buy item: ")
+mooney.buy(100,Health_Potion,Flimsy_Wooden_Sword,Flimsy_Steel_Sword)
