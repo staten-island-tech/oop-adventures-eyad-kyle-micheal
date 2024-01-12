eaa1642ea605438db0ff7b039882411ba99059d7
mellowed24 <eyadabdelfatah6@gmail.com>
2024-01-11 22:40:22 -0500

    bjk

--- main_project.py
+++ main_project.py
@@ -5,9 +5,9 @@ def tutorial_tiny():
 Player.print_classes()
 chosen = Player.better_choose_class()
 def actual():
-    print(f"""Great! You've sucessfullay chosen a class ! Here aare your stats 
-          {chosen.__dict__} """)
-    print(f"Now that you've chosen a class, you should learn how to fight. Here's a goblin to start with")
+    print(f"""Great! You've sucessfullay chosen a class ! Here aare your stats \n {chosen.__dict__} """)
+    print(f'The "attack" stat is a multiplier as to how much damage you will do. For example, since you have an attak stat of {chosen.attack} you will deal {chosen.attack} times damage on you attacks. That being said, these attacks will also go through the enemies defense stat,meaning that if you do an attack of 10 damage, you will actually do an attack of 10*{chosen.attack} * enenmy defense.')
+    print(f"\nNow that you've chosen a class, you should learn how to fight. Here's a goblin to start with")
     Floors.encounter(goblin,chosen,5)
     
 
---
+++ test.py
@@ -0,0 +1,5 @@
+def re():
+    return 2
+e = re()
+print(e)
+
